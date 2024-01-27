8bff6e29f0f848bfde57e74117517d83f33ce7f3
mathewnicholls <mathewnicholls@yandex.com>
2024-01-27 11:30:05 +0000

    readding backend state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,11 @@ provider "google" {
   project = var.project_id
   region = var.region
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "osrs-track-terraform-state"
+    prefix = "terraform/state"
+    credentials = "account.json"
+  }
+}
