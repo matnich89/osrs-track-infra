a4a06706a55e9370ecf8e53acdfa27c45e02e46b
mathewnicholls <mathewnicholls@yandex.com>
2024-01-27 06:57:43 +0000

    remove space

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -37,7 +37,6 @@ resource "google_container_node_pool" "primary_node" {
     preemptible  = false
     machine_type = "e2-standard-2"
     service_account = google_service_account.osrs-track-service-account.email
-
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
